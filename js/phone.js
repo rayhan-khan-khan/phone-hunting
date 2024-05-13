7749e4d7ac4cb0c53d4a6fc4af5c565fa7e414ec
Rayhan khan <rayhan.khanlaze@gmail.com>
2024-05-13 17:24:52 +0600

    done

--- js/phone.js
+++ js/phone.js
@@ -1,7 +1,5 @@
-const loadPhone = async () => {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
-  );
+const loadPhone = async (searchPhone) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchPhone}`);
   const data = await res.json();
   const phones = data.data;
   displayPhones(phones);
@@ -10,12 +8,13 @@ const loadPhone = async () => {
 const displayPhones = (phones) => {
   // main div
   const mainContainer = document.getElementById("div-container");
+  mainContainer.textContent = '';
   phones.forEach((element) => {
     console.log(element);
     const phoneDiv = document.createElement("div");
-    phoneDiv.classList = "card mx-auto bg-base-100 shadow-xl";
+    phoneDiv.classList = "card w-72 md:w-80 lg:w-96 border-2 hover:border-purple-600 mx-auto bg-base-100 shadow-xl";
     phoneDiv.innerHTML = `
-    <figure><img class="p-10" src="${element.image}" alt="Shoes" /></figure>
+    <figure><img class="md:p-10 p-5" src="${element.image}" alt="Shoes" /></figure>
     <div class="card-body">
       <h2 class="card-title mx-auto">${element.phone_name}</h2>
       <div class="card-actions justify-center text-center">
@@ -29,5 +28,12 @@ const displayPhones = (phones) => {
     mainContainer.appendChild(phoneDiv)
   });
 };
+
+
+const handleSearch = ()=>{
+    const inputField = document.getElementById('search-field');
+    const input = inputField.value;
+    loadPhone(input)
+}
 
 loadPhone();
